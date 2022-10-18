47d534e9c46426506da56868cb126ffabaeeaa33
Austin Sohn <sohn.austin@gmail.com>
2022-10-17 17:37:47 -0700

    added sample code

--- README.md
+++ README.md
@@ -1,1 +1,32 @@
 # 254-group13
+
+# Install PyQT(PySide6)
+
+- https://doc.qt.io/qtforpython/quickstart.html#requirements
+
+# Documentation
+
+- https://doc.qt.io/qtforpython/contents.html
+
+# How to update GitHub
+
+- git add remote origin https://github.com/austin-sohn/254-group13/
+- git checkout -b <new branch name>
+  - creates a new local branch
+- git add <files>
+  - adds the files to be sent.
+  - use "git add ." to add all the files in the folder
+- git commit -m "some message"
+  - writes a commit messages
+- git push -u origin <new branch name>
+  - make sure to be in the correct local branch before pushing new stuff
+  - pushes the files to the repository under the new branch name
+
+# Other useful git commands
+
+- Check what local branches you have
+  - git branch
+- Switch to different branches
+  - git checkout <branch name>
+- Delete local branch
+  - git branch -d <branch name>
---
+++ main.py
@@ -0,0 +1,36 @@
+# Austin Sohn, Justin Sohn, Samuel Sandoval
+# main.py
+
+import sys
+import random
+from PySide6 import QtCore, QtWidgets, QtGui
+
+class GUI(QtWidgets.QWidget):
+    def __init__(self):
+        super().__init__()
+
+        self.hello = ["Hallo Welt", "Hei maailma", "Hola Mundo", "Привет мир"]
+
+        self.button = QtWidgets.QPushButton("Click me!")
+        self.text = QtWidgets.QLabel("Hello World",
+                                     alignment=QtCore.Qt.AlignCenter)
+
+        self.layout = QtWidgets.QVBoxLayout(self)
+        self.layout.addWidget(self.text)
+        self.layout.addWidget(self.button)
+
+        self.button.clicked.connect(self.magic)
+
+    @QtCore.Slot()
+    def magic(self):
+        self.text.setText(random.choice(self.hello))
+
+def main():
+  app = QtWidgets.QApplication([])
+  widget = GUI()
+  widget.resize(800, 600)
+  widget.show()
+
+  sys.exit(app.exec())
+if __name__ == "__main__":
+  main()
