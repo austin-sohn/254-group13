830e9b2d9c82f3466053e067acd7269722361a9e
Austin Sohn <sohn.austin@gmail.com>
2022-11-06 18:45:52 -0800

    fixed table bug

--- database/database.py
+++ database/database.py
@@ -7,16 +7,16 @@ class DatabaseClass():
     super().__init__()
     self.tableName = "tasks"
     self.db = sqlite_utils.Database("./database/tasks.db")
-    self.table = self.db.table(self.tableName, pk="task_id", 
+    self.taskTable = self.db.table(self.tableName, pk="task_id", 
     columns={"task_id": int, "start_date": date, "end_date": date, "status": bool},
     column_order=("task_id","task","start_date","end_date","status"))
 
 # checks the table to see if it exsits
   def checkTable(self, params):
     if type(params) is dict:
-      self.count = self.table.count_where("task_id = " + str(params.get("task_id")))
+      self.count = self.taskTable.count_where("task_id = " + str(params.get("task_id")))
     else:
-      self.count = self.table.count_where("task_id = " + str(params))
+      self.count = self.taskTable.count_where("task_id = " + str(params))
     if self.count > 0:
       return True
     return False
@@ -36,7 +36,7 @@ class DatabaseClass():
 # adds task to database after checking it exists or not
   def addTask(self, params):
     if not self.checkTable(params):
-      self.table.insert(params)
+      self.taskTable.insert(params)
       print("Added")
       self.outputDB()
 
@@ -44,7 +44,7 @@ class DatabaseClass():
   def removeTask(self, params):
     try:
       if self.checkTable(params):
-        self.table.delete(params)
+        self.taskTable.delete(params)
         print("Deleted")
         self.outputDB()
     except:
