80525efeec62aeb47ea90e90092b2741e952d8e2
Austin Sohn <sohn.austin@gmail.com>
2022-11-24 21:15:23 -0800

    added subtask database

--- database/database.py
+++ database/database.py
@@ -6,54 +6,66 @@ class DatabaseClass():
   def __init__(self):
     super().__init__()
     self.taskTableName = "tasks"
-    self.db = sqlite_utils.Database("./database/tasks.db")
+    self.db = sqlite_utils.Database("./database/database.db")
+    self.subtaskTableName = "subtasks"
     self.taskTable = self.db.table(self.taskTableName, pk="task_id", 
     columns={"task_id": int, "start_date": date, "end_date": date, "status": bool},
     column_order=("task_id","task","start_date","end_date","status"))
+    self.subtaskTable = self.db.table(self.subtaskTableName, pk="subtask_id", 
+    columns={"subtask_id": int, "id":int, "start_date": date, "end_date": date, "status": bool},
+    column_order=("task_id","id","task","start_date","end_date","status"))
 
 # checks the table to see if it exsits
-  def checkTable(self, params):
+  def checkTable(self, table, params):
     if type(params) is dict:
-      self.count = self.taskTable.count_where("task_id = " + str(params.get("task_id")))
+      count = self.db[table].count_where("task_id = " + str(params.get("task_id")))
     else:
-      self.count = self.taskTable.count_where(f"task_id = {str(params)}")
-    return self.count > 0
+      count = self.db[table].count_where(f"task_id = {str(params)}")
+    return count > 0
 
-# outputs the entire database
-  def outputDB(self):
-    for row in self.db.query(f"SELECT * FROM {self.taskTableName}"):
+# outputs the entire database. only for testing.
+  def outputDB(self, table):
+    for row in self.db.query(f"SELECT * FROM {table}"):
       print(row)
 
 # get list of database
-  def listDB(self):
+  def listDB(self, table):
     l = []
-    for row in self.db.query("SELECT * FROM " + self.taskTableName):
+    for row in self.db.query(f"SELECT * FROM {table}"):
       l.append(row)
     return l
 
 # adds task to database after checking it exists or not
-  def addTask(self, params):
-    if not self.checkTable(params):
-      self.taskTable.insert(params)
+  def addTask(self, table, params):
+    if not self.checkTable(table, params):
+      self.db[table].insert(params)
       print("Added")
-      self.outputDB()
+      # self.outputDB(table)
 
 # removes the task
-  def removeTask(self, params):
+  def removeTask(self, table, params):
     try:
-      if self.checkTable(params):
+      if self.checkTable(table, params):
         self.taskTable.delete(params)
         print("Deleted")
-        self.outputDB()
+        # self.outputDB(table)
     except:
       print("error in removeTask")
 
 # find highest task_id
-  def highestTaskID(self):
-    cur = self.db.execute("SELECT MAX(task_id) FROM tasks").fetchall()
+  def highestTaskID(self, table):
+    if table == "tasks":
+      cur = self.db.execute("SELECT MAX(task_id) FROM tasks").fetchall()
+    elif table == "subtasks":
+      cur = self.db.execute("SELECT MAX(subtask_id) FROM subtasks").fetchall()
+    else:
+      print("wrong table name")
     return cur[0][0]
 
-  def findTaskID(self, task):
-    cur = self.db.execute("SELECT task_id FROM tasks WHERE task = ?", [task]).fetchall()
+  def findTaskID(self, table, task):
+    if table == "tasks":
+      cur = self.db.execute("SELECT task_id FROM tasks WHERE task = ?", [task]).fetchall()
+    elif table == "subtasks":
+      cur = self.db.execute("SELECT subtask_id FROM subtasks WHERE subtask = ?", [task]).fetchall()
     if cur:
       return int(cur[0][0])
--- run.sh
+++ run.sh
@@ -1,2 +1,6 @@
-sqlite-utils insert ./database/tasks.db tasks ./database/tasks.csv --csv --pk=task_id --ignore
+sqlite-utils create-database ./database/database.db
+sqlite-utils create-table ./database/database.db tasks task_id integer --pk=task_id
+sqlite-utils insert ./database/database.db tasks ./database/tasks.csv --csv --ignore --alter
+sqlite-utils create-table ./database/database.db subtasks subtask_id integer --pk=subtask_id
+sqlite-utils insert ./database/database.db tasks ./database/subtasks.csv --csv --ignore --alter
 python3 main.py
